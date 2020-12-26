fdf749a94f4294dcaeeb4a6dd70612a41b4d1b1d
yechu <yechukim217@gmail.com>
2020-12-26 19:30:11 +0900

    separate world-time class added, try& catch added

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -11,22 +11,32 @@ class WorldTime {
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async {
-    //loading.dart의 getTime()에 await를 붙일때..
-    //Future를 async앞에 붙여줘야 한다.
 
-    //make request
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body); // 데이터가 저장되어 있는 body를 디코딩
+    try {
+      //loading.dart의 getTime()에 await를 붙일때..
+      //Future를 async앞에 붙여줘야 한다.
 
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    print(offset);
 
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      //make request
+      Response response =
+          await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body); // 데이터가 저장되어 있는 body를 디코딩
 
-    //set the time property
-    time = now.toString();
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      print(offset);
+
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      //set the time property
+      time = now.toString();
+    } catch (e) {
+      //error msg
+      print('caught error: $e');
+      //time = null되니까 에러 메시지 길게 뜸 다음과 같이 time을 설정해줌
+      time = 'could not get time data';
+    }
   }
 }
 
