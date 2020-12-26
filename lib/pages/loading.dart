ee0a9281bcadfe9e1561967ca5aeca76d5211b35
yechu <yechukim217@gmail.com>
2020-12-26 19:05:55 +0900

    separate world time class added

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,32 +8,32 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async{
-    //make request
-    Response response = await get('http://worldtimeapi.org/api/timezone/Asia/Seoul');
-    Map data = jsonDecode(response.body); // 데이터가 저장되어 있는 body를 디코딩
-    //print(data);
+  String time = 'Loading';
 
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-    print(offset);
-
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
-
+  void setupWorldTime() async{ // await 를 사용하려면 async 붙여야 한다는 걸 기억하자
+    WorldTime instance = WorldTime(
+        location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime(); //인스턴스 만들고 시간가져오는 메소드 호출할 수 있음.
+    //getTime()은 async 라는 걸 기억해야 한다. await를 붙여서 바로 print되지 못하게 만든다.
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
---
+++ lib/services/world_time.dart
@@ -0,0 +1,34 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+  String location; //location name for the UI
+  String time; // the time in that location
+  String flag; // url to an asset flag icon
+  String url; // location url for api endpoint
+
+  //네임 파라미터
+  WorldTime({this.location, this.flag, this.url});
+
+  Future<void> getTime() async {
+    //loading.dart의 getTime()에 await를 붙일때..
+    //Future를 async앞에 붙여줘야 한다.
+
+    //make request
+    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+    Map data = jsonDecode(response.body); // 데이터가 저장되어 있는 body를 디코딩
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    print(offset);
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    //set the time property
+    time = now.toString();
+  }
+}
+
+//WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url:'Europe/Berlin');
+// eg) instance.getTime();
