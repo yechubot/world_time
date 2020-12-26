95381c802b9f77a89a3e10cf1f52c3aacd698971
yechu <yechukim217@gmail.com>
2020-12-26 18:42:46 +0900

    get data from API

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -9,17 +9,26 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async{
+  void getTime() async{
+    //make request
+    Response response = await get('http://worldtimeapi.org/api/timezone/Asia/Seoul');
+    Map data = jsonDecode(response.body); // 데이터가 저장되어 있는 body를 디코딩
+    //print(data);
 
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-    Map data = jsonDecode(response.body);
-    print(data);
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    print(offset);
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
